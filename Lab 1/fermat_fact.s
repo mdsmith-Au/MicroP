a75a0eea48611ca46815066b898aba610a05ddb6
Michael Smith <michael.smith6@mail.mcgill.ca>
2014-01-22 18:44:39 -0500

    Fixed bug in find square that violated guarantee of int(y); changed guarantee to R12
    Former-commit-id: 23142a54f743f8e5439b9edf12073a3f9d83826e
    Former-commit-id: df157ded40ffcde64a64c3d5a7c213195135e030

--- Lab 1/fermat_fact.s
+++ Lab 1/fermat_fact.s
@@ -1,35 +1,55 @@
+;****************************
+; Fermat Factorization Algorithm
+; Takes an integer as input in register R1
+; and finds the factors if possible.
+;
+; Uses registers **** to ***
+;
+; By Michael Smith / Kevin Dam
+; 260481943 / ********
+;
+	
 	area fermat_factorization, code, readonly
 	export fermat_fact
 
 fermat_fact
 	; Number to be factored
+	; TODO: Parameter passing
 	movs  R1, #21
 	
-	; movs by default compares against 0
-	blt   stop
-	beq   stop
+	; movs by default compares against 0,
+	; so we can use less than, equal to, etc.
+	; Below: Exit if number <= 0
+	ble   stop
 	
-	; Even case
+	; OK; number is > 0
+	; Now, check if number is even/odd using
+	; the last bit
 	tst   R1, #1
+	
+	; Number is even, don't need Fermat
 	beq   even_case
 	
-	; Odd case (Fermat factorization algorithm)
+	; Number is odd -> Fermat factorization algorithm
 	
 	; Set FPSCR flag RMode to ceil
+	; Follows example from ARM
+	; See http://infocenter.arm.com/help/topic/com.arm.doc.dui0473c/DUI0473C_using_the_arm_assembler.pdf, Ex. 5.19.1
 	vmrs R7, FPSCR
 	bic R7, R7, #0x00c00000
 	orr R7, R7, #0x00400000
 	vmsr FPSCR, R7
 	
-	; Converts integer to IEEE 754 floating point
-	vmov.F32        S1, R1
-	vcvt.F32.U32    S1, S1
+	; Converts integer in R1 to IEEE 754 floating point in S0
+	vmov.F32        S0, R1
+	vcvt.F32.U32    S0, S0
 	
-	vsqrt.F32       S2, S1
+	; Calculate square root
+	vsqrt.F32       S0, S0
 	 
-	; Floating point to integer
-	vcvtr.U32.F32 S2, S2
-	vmov.F32 R2, S2 ; x = ceil(sqrt(N))
+	; Convert answer back to integer, store in R2
+	vcvtr.U32.F32 S0, S0
+	vmov.F32 R2, S0 ; x = ceil(sqrt(N))
 	
 	; Set FPSCR flag RMode back to default
 	vmrs R7, FPSCR
@@ -51,16 +71,16 @@ is_not_square
 	
 
 factorsoln
-	add R5, R2, R11
-	sub R6, R2, R11
+	; F1, F2 in R5, R6 respectively
+	add R5, R2, R12
+	sub R6, R2, R12
 	
 	bx lr	
 	
 find_square 
 	; Finding if a num is square
-	; Uses S10 for floating point calculations
-	; Set R10 to the number to check
-	; Guarantees R11 to hold value of int(y)
+	; Takes R10 as input, writes to R11, R12 and S10
+	; Guarantees R12 to hold value of int(y)
 	
 	; Grab R10, convert to IEE 754
 	vmov.F32		S10, R10
@@ -69,8 +89,11 @@ find_square
 	vsqrt.F32		S10, S10
 	
 	vcvt.U32.F32	S10, S10
-	vmov.F32		R11, S10	; R11 is now the integer of the square root (int(y))
+	vmov.F32		R11, S10
 	
+	; Copy int(y) to a seperate register for (possible) usage later
+	mov				R12, R11	
+
 	mul				R11, R11, R11  ; R11 = int(y) * int(y)
 	cmp				R11, R10       ; compares int(y) * int(y) and y^2
 	beq				factorsoln		; if R11 == R10, we are done, goto factorsoln
