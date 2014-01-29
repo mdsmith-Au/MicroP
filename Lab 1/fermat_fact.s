71ff1b70d0a62cf80dd5a426f83d4a8732965395
Michael Smith <michael.smith6@mail.mcgill.ca>
2014-01-29 13:13:38 -0500

    Rewrote fermat in assembly to use less registers.  Also wrote assembly testbench, although it doesn't work
    Former-commit-id: 7cf88c3979437e6c7668ce3e1eb27a9af7878022
    Former-commit-id: 8f11b9580fc85a9f6944f2dc4bccea0b175a4bc4

--- Lab 1/fermat_fact.s
+++ Lab 1/fermat_fact.s
@@ -1,17 +1,16 @@
 ; *******************************************************
 ; Fermat Factorization Algorithm
 ;
-; Takes an integer as input in register R1 (N)
+; Takes an integer as input in register R0 (N)
 ; and finds the factors if possible.
 ;
 ; Input registers:
-;   R1     holds an integer N
+;   R0     holds an integer N
 ; Output registers:
-;   R5     holds the first factor F1 
-;   R6     holds the second factor F2
+;   R0     holds the first factor F1 
+;   R1     holds the second factor F2
 ;
-; Uses registers R1 to R3, R5, R6, R7, R10 - R12
-; Uses FPU registers S0, S10
+; USES REGISTERS *********************************
 ;
 ; By: Michael Smith | Kevin Dam
 ;         260481943 | 260457383
@@ -21,17 +20,14 @@
 	export fermat_fact
 
 fermat_fact
-	; Number to be factored
-	; TODO: Parameter passing
-	mov             R1, #53817
 	
 	; Check if N <= 0. If yes, exit.
-	cmp             R1, #0
+	cmp             R0, #0
 	ble             stop
 	
 	; N > 0 is guaranteed to be true
 	; Checks the LSB to determine parity of N
-	tst             R1, #1
+	tst             R0, #1
 	
 	; Number is even, don't need Fermat
 	beq             even_case
@@ -41,53 +37,50 @@ fermat_fact
 	; Set FPSCR flag RMode (rounding mode) to round up (ceil function)
 	; Follows example from ARM; See Ex. 5.19.1 of
 	; http://infocenter.arm.com/help/topic/com.arm.doc.dui0473c/DUI0473C_using_the_arm_assembler.pdf,
-	; We reserve R7 in this subroutine for FPSCR configuration
-	vmrs            R7, FPSCR              ; loads FPSCR flags to temp register
-	bic             R7, R7, #0x00c00000    ; clears RMode flag bits
-	orr             R7, R7, #0x00400000    ; sets RMode bits to round up mode
-	vmsr            FPSCR, R7              ; stores modified flags to FPSCR
+	vmrs            R1, FPSCR              ; loads FPSCR flags to temp register
+	bic             R1, R1, #0x00c00000    ; clears RMode flag bits
+	orr             R1, R1, #0x00400000    ; sets RMode bits to round up mode
+	vmsr            FPSCR, R1              ; stores modified flags to FPSCR
 	
-	; Convert integer in R1 to IEEE 754 floating point in S0
-	vmov.F32        S0, R1
+	; Convert integer in R0 to IEEE 754 floating point in S0
+	vmov.F32        S0, R0
 	vcvt.F32.U32    S0, S0
 	
 	; Calculate square root
 	vsqrt.F32       S0, S0
 	 
-	; Convert result back to integer, store in R2
+	; Convert result back to integer, store in R1
 	; Uses FPSCR RMode flag (set to round up)
 	vcvtr.U32.F32   S0, S0
-	vmov.F32        R2, S0                 ; x = ceil(sqrt(N))
+	vmov.F32        R1, S0                 ; R1 = x = ceil(sqrt(N))
 	
 	; Set FPSCR flag RMode back to default
-	vmrs            R7, FPSCR              ; copies FPSCR flags to temp register
-	bic             R7, R7, #0x00c00000    ; clears RMode flag bits
-	vmsr            FPSCR, R7              ; stores modified flags to FPSCR
+	vmrs            R2, FPSCR              ; copies FPSCR flags to temp register
+	bic             R2, R2, #0x00c00000    ; clears RMode flag bits
+	vmsr            FPSCR, R2              ; stores modified flags to FPSCR
 	
-	mul             R3, R2, R2             ; R3 = x * x 
-	sub             R3, R3, R1             ; R3 = y^2 = x*x - N)
+	mul             R2, R1, R1             ; R2 = x * x 
+	sub             R2, R2, R0             ; R2 = y^2 = x*x - N)
 	
 loop
-	mov             R10, R3
 	b               find_square
 	
 is_not_square
-	add             R2, R2, #1             ; x = x + 1
-	mul             R3, R2, R2             ; Replace old y^2 with new y^2 = x * x
-	sub             R3, R3, R1             ; y^2 = x * x - N
+	add             R1, R1, #1             ; x = x + 1
+	mul             R2, R1, R1             ; Replace old y^2 with new (partial) y^2 = x * x
+	sub             R2, R2, R0             ; y^2 = x * x - N
 	b               loop
 	
 
 factorsoln
-	add             R5, R2, R12            ; F1 = R5 = x + y
-	sub             R6, R2, R12            ; F2 = R6 = x - y
+	add             R0, R1, R4            ; F1 = R0 = x + y
+	sub             R1, R1, R4            ; F2 = R1 = x - y
 	
 	bx              lr	
 	
 find_square 
 	; Finding if a num is square
-	; Takes R10 as input, writes to R11, R12 and S10
-	; Guarantees R12 to hold value of int(y)
+	; Takes R2 as input, writes to **************************************
 	
 	; Idea: take an integer, find the square root,
 	; convert it to an integer (stripping everything after
@@ -97,34 +90,34 @@ find_square
 	; the result is different because information was lost
 	; during the conversion - hence, it is not a square.
 	
-	; Grab R10, convert to IEEE 754
-	vmov.F32		S10, R10
-	vcvt.F32.U32	S10, S10
+	; Grab R2, convert to IEEE 754
+	vmov.F32		S0, R2
+	vcvt.F32.U32	S0, S0
 	
-	vsqrt.F32		S10, S10               ; S10 = sqrt(R10)
+	vsqrt.F32		S0, S0	              ; S10 = sqrt(R10)
 	
 	; Convert sqrt to integer: int(y)
-	vcvt.U32.F32	S10, S10
-	vmov.F32		R11, S10
+	vcvt.U32.F32	S0, S0
+	vmov.F32		R3, S0
 	
 	; Copy int(y) to a seperate register for (possible) usage later
-	mov				R12, R11	
+	mov				R4, R3	
 
-	mul				R11, R11, R11          ; R11 = int(y) * int(y)
-	cmp				R11, R10               ; compares int(y) * int(y) and y^2
-	beq				factorsoln		       ; if R11 == R10, we are done, goto factorsoln
-	b				is_not_square          ; if R11 != R10, run the loop
+	mul				R3, R3, R3         		 ; R3 = int(y) * int(y)
+	cmp				R2, R3              	 ; compares int(y) * int(y) and y^2
+	beq				factorsoln		     	 ; if R3 == R2, we are done, goto factorsoln
+	b				is_not_square         	 ; if R3 != R2, run the loop
 	
 even_case
-	; Set R5 (F1) to 2
-	; Set R6 (F2) to N/2 by shifting, return
-	mov             R5, #2
-	lsr             R6, R1, #1
+	; Set R0 (F1) to 2
+	; Set R1 (F2) to N/2 by shifting, return
+	lsr             R1, R0, #1
+	mov             R0, #2
 	bx              lr
 	
 stop
-	; Set R5, R6 to 0 (F1, F2), return
-	mov             R5, #0
-	mov             R6, #0
+	; Set R0, R1 to 0 (F1, F2), return
+	mov             R0, #0
+	mov             R1, #0
 	bx	            lr
 	end
---
+++ Lab 1/src/assembly_testbench.s
@@ -0,0 +1,135 @@
+	AREA assembly_testbench, CODE, READONLY
+	EXPORT assembly_testbench
+	
+	IMPORT fermat_fact
+	; N - number to be factored - is placed in R0 before execution
+	; F1, F2 are R0, R1 after execution
+	
+	; Load 2 into R0, execute Fermat
+	MOV		R0, #2
+	LDR     R1, =fermat_fact
+	BLX		R1
+	
+	; Verify Fermat got good factors
+	MUL		R0, R0, R1
+	CMP		R0, #2
+	BNE		error
+	
+	; Rinse and repeat for other numbers, 11 in this csae
+	MOV 	R0, #11
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #11
+	BNE		error
+	
+	; 8
+	MOV 	R0, #8
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #8
+	BNE		error
+	
+	; 24
+	MOV 	R0, #24
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #24
+	BNE		error
+	
+	; 87
+	MOV 	R0, #87
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #87
+	BNE		error
+	
+	; 88
+	MOV 	R0, #88
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #88
+	BNE		error
+	
+	;2876
+	MOV 	R0, #2876
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	MOV		R1, #2876
+	CMP		R0, R1
+	BNE		error
+	
+	; 0
+	MOV 	R0, #0
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #0
+	BNE		error
+	
+	; 1
+	MOV 	R0, #1
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #1
+	BNE		error
+	
+	; -985
+	MOV 	R0, #-985
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	MOV		R1, #-985
+	CMP		R0, R1
+	BNE		error
+	
+	;25985
+	MOV 	R0, #25985
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	MOV		R1, #25985
+	CMP		R0, R1
+	BNE		error
+	
+	;2^23
+	MOV 	R0, #8388608
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	CMP		R0, #8388608
+	BNE		error
+	
+	;787993
+	MOV32 	R0, #787993
+	LDR		R1, =fermat_fact
+	BLX		R1
+	
+	MUL		R0, R0, R1
+	MOV32	R1, #787993
+	CMP		R0, R1
+	BNE		error
+	
+	
+	BX		lr
+	
+error	NOP
+		BX lr
+		end
