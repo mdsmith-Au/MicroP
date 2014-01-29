5a8ef9baf40a0f621e151b9fc6bf9bdc2f9a1338
Kevin Dam <kevin.dam@mail.mcgill.ca>
2014-01-29 17:25:38 -0500

    Started prime factorization
    Former-commit-id: f81ac41524401e9c1eaa01d54ce49a0d597e89bb
    Former-commit-id: 1c7587d7db010a3a41029678647c6f3b18f0a5d8

---
+++ Lab 1/src/primefactorization.c
@@ -0,0 +1,22 @@
+/**
+ * Prime factorization algorithm
+ */
+ 
+#include <string.h>
+ 
+int find_prime_factors(int n) {
+	int prime_factors[32];
+	
+	/* Initializes array to 0 */
+	memset(prime_factors, 0, sizeof(prime_factors)/sizeof(int));
+	
+	
+	
+	
+	return 0;
+}
+
+int is_prime(int n) {
+	
+}
+
