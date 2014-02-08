cff11f043ddd0bdd13ca593f8a11dd7bf5d2a1c4
Michael Smith <michael.smith6@mail.mcgill.ca>
2014-02-08 15:20:58 -0500

    Testing code for display
    Former-commit-id: 711ea5a733e44fc018c8a3a9341f3a231e36b6ec
    Former-commit-id: 617696662285c45998bd05d62fc37692f4126c6e

--- Lab 2/src/gpio_example.c
+++ Lab 2/src/gpio_example.c
@@ -5,7 +5,7 @@ void GPIO_example_config(void){
 	
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; 
