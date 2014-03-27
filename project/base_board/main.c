2a74990b8730558ef7af6a5dd4cb0dee6895648f
d4z3d <nicholas.destounis@mail.mcgill.ca>
2014-03-27 11:50:27 -0400

    motors driver is done

--- project/base_board/main.c
+++ project/base_board/main.c
@@ -2,11 +2,25 @@
 #include "stm32f4xx.h"
 #include "cmsis_os.h"
 
+#include "../common/motors_driver.h"
+
 /*!
  @brief Program entry point
  */
 int main (void) {
+	init_motors();
 	
+	roll_move_to_angle(0);
+	pitch_move_to_angle(0);
+	
+	roll_move_to_angle(45);
+	pitch_move_to_angle(90);
+	
+	roll_move_to_angle(90);
+	pitch_move_to_angle(45);
+	
+	roll_move_to_angle(0);
+	pitch_move_to_angle(0);
 	
 	// The below doesn't really need to be in a loop
 	while(1){
